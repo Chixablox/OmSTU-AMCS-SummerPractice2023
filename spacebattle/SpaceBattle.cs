b625510e26d6b81787c8a2ccd62691ee04b9f1d8
Chixablox <smrdak152@gmail.com>
2023-07-01 08:08:16 +0000

    V1

--- spacebattle/SpaceBattle.cs
+++ spacebattle/SpaceBattle.cs
@@ -5,7 +5,11 @@ public class SpaceShip
     public double[] position = new double[2]{double.NaN, double.NaN};
     public double[] speed = new double[2]{double.NaN, double.NaN};
     public bool possibility_of_movement = true;
-
+    public double fuelQuantity = 0;
+    public double fuelFlow = double.NaN;
+    public double angleOfInclination = 0;
+    public double angularSpeed = double.NaN;
+    public bool abilityToChangeTheAngle = true;
     public SpaceShip()
     {}
 
@@ -17,8 +21,25 @@ public class SpaceShip
         this.speed = speed;
     }
 
-    public void SetSpaceShipPossOfMove(bool possibility_of_movement){
+    public void SetSpaceShipPossOfMove (bool possibility_of_movement){
         this.possibility_of_movement = possibility_of_movement;
+    }
+
+    public void SetSpaceShipFuelQuantity (double fuelQuantity){
+        this.fuelQuantity = fuelQuantity;
+    }
+    public void SetSpaceShipAngleOfInclination(double angleOfInclination){
+        this.angleOfInclination = angleOfInclination;
+    }
+    public void SetSpaceShipFuelFlow(double fuelFlow){
+        this.fuelFlow = fuelFlow;
+    }
+    public void SetSpaceShipAngularSpeed(double angularSpeed){
+        this.angularSpeed = angularSpeed;
+    }
+
+    public void SetSpaceShipAbilityToChangeTheAngle(bool abilityToChangeTheAngle){
+        this.abilityToChangeTheAngle = abilityToChangeTheAngle;
     }
 
     public double[] Movement (){
@@ -36,5 +57,32 @@ public class SpaceShip
             position[1] += speed[1];
             return position;
         }
+    }
+    public double MovementWithFuel (){
+
+        if((fuelQuantity-fuelFlow)<=(-1e-5)){
+            throw new Exception();
+        }
+        else{
+            fuelQuantity -= fuelFlow;
+            return fuelQuantity;
+        }
+    }
+    public double RotationAroundAxis (){
+
+        if(double.IsNaN(angleOfInclination)){
+            throw new Exception();
+        }
+        else if(double.IsNaN(angularSpeed)){
+            throw new Exception();
+        }
+        else if(abilityToChangeTheAngle == false){
+            throw new Exception();
+        }
+        else{
+            angleOfInclination += angularSpeed;
+            return angleOfInclination;
+        }
+
     }
  }
--- spacebattletests/SpaceBattleTests.cs
+++ spacebattletests/SpaceBattleTests.cs
@@ -3,11 +3,9 @@ using SpaceBattle;
 using TechTalk.SpecFlow;
 
 
-[Binding]
-public class StepDefinitions
+[Binding,Scope(Feature = "Перемещение корабля")]
+public class MovmentTests
 {
-    /*private double[] coords = new double[2];
-    private double[] speed = new double[2];*/
     private double[] coords = new double[2];
     private Exception r_exp = new Exception();
     private SpaceShip spaceShip = new SpaceShip();
@@ -68,11 +66,144 @@ public class StepDefinitions
         double[] r_ans = new double[] {x1, x2};
         bool result = false;
         
-        if (r_ans[0]==coords[0] & r_ans[1]==coords[1])
+        if (Math.Abs(r_ans[0]-coords[0])<1e-5 & Math.Abs(r_ans[1]-coords[1])<1e-5)
         {
             result = true;
         }
 
         Assert.True(result, "Incorrect");
     }
+}
+
+
+
+[Binding,Scope(Feature = "Движение с расходом топлива")]
+public class MovementWithFuel
+{
+    private double fuelQuantity;
+    private Exception r_exp = new Exception();
+    private SpaceShip spaceShip = new SpaceShip();
+    [When("происходит прямолинейное равномерное движение без деформации")]
+    public void CalculatedTheMovementWithFuelOfTheSpaceShip()
+    {
+        try 
+        {
+            fuelQuantity = spaceShip.MovementWithFuel();
+        }
+        catch (Exception e)
+        {
+            r_exp =  e;
+        }
+    }
+
+    [Given(@"космический корабль имеет топливо в объеме (.*) ед")]
+    public void GivenTheFuelQuantityOfTheSpaceship(double x) 
+    {
+        double in_fuelQuantity = x;
+        spaceShip.SetSpaceShipFuelQuantity(in_fuelQuantity);
+    }
+
+    [Given(@"имеет скорость расхода топлива при движении (.*) ед")]
+    public void GivenTheFuelFlow(double x_speed) 
+    {
+        double in_fuelFlow = x_speed; 
+        spaceShip.SetSpaceShipFuelFlow(in_fuelFlow);
+    }
+
+    [Then(@"новый объем топлива космического корабля равен (.*) ед")]
+    public void CalculationOfTheNewFuelVolumeOfTheSpaceship(double x1)
+    {
+        double r_ans = x1;
+        bool result = false;
+        
+        if (Math.Abs(fuelQuantity-x1)<1e-5)
+        {
+            result = true;
+        }
+
+        Assert.True(result, "Incorrect");
+    }
+
+    [Then(@"возникает ошибка Exception")]
+    public void ThrowingException()
+    {
+        Assert.ThrowsAsync<Exception>(() => throw r_exp);
+    }
+}
+
+[Binding,Scope(Feature = "Вращение коробля")]
+public class RotationAroundAxisTests
+{
+    private double angleOfInclination;
+    private Exception r_exp = new Exception();
+    private SpaceShip spaceShip = new SpaceShip();
+    
+    [When("происходит вращение вокруг собственной оси")]
+    public void CalculatedRotationAroundItsOwnAxisOfTheSpaceShip()
+    {
+        try 
+        {
+            angleOfInclination = spaceShip.RotationAroundAxis();
+        }
+        catch (Exception e)
+        {
+            r_exp =  e;
+        }
+    }
+
+    [Given(@"космический корабль имеет угол наклона (.*) град к оси OX")]
+    public void GivenAngleOfInclinationOfTheSpaceShip(double x) 
+    {
+        double in_angleOfInclination = x;
+        spaceShip.SetSpaceShipAngleOfInclination(in_angleOfInclination);
+    }
+
+    [Given(@"имеет мгновенную угловую скорость (.*) град")]
+    public void GivenTheAngularSpeedOfTheSpaceShip(double x_speed) 
+    {
+        double in_angularSpeed = x_speed; 
+        spaceShip.SetSpaceShipAngularSpeed(in_angularSpeed);
+    }
+
+    [Given("невозможно изменить уголд наклона к оси OX космического корабля")]
+    public void GivenTheFalseAbilityToChangeTheAngleOfTheSpaceShip() 
+    {
+        bool in_ability = false; 
+        spaceShip.SetSpaceShipAbilityToChangeTheAngle(in_ability);
+    }
+
+    [Given("космический корабль, угол наклона которого невозможно определить")]
+    public void GivenTheUndefinedAngleOfTheSpaceShip() 
+    {
+        double in_angleOfInclination = double.NaN;
+        spaceShip.SetSpaceShipAngleOfInclination(in_angleOfInclination);
+    }
+
+    [Given("мгновенную угловую скорость невозможно определить")]
+    public void GivenTheUndefinedAngulanSpeedOfTheSpaceShip() 
+    {
+        double in_angularSpeed = double.NaN; 
+        spaceShip.SetSpaceShipAngularSpeed(in_angularSpeed);
+    }
+
+    [Then(@"угол наклона космического корабля к оси OX составляет (.*) град")]
+    public void ThrowingException(double x1)
+    {
+        double r_ans = x1;
+        bool result = false;
+        
+        if (Math.Abs(angleOfInclination-x1)<1e-5)
+        {
+            result = true;
+        }
+
+        Assert.True(result, "Incorrect");
+    }
+
+    [Then(@"возникает ошибка Exception")]
+    public void ThrowingException()
+    {
+        Assert.ThrowsAsync<Exception>(() => throw r_exp);
+    }
+
 }
