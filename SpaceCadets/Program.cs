be6a05a11fabd42c802f8ef36188233bbe3901d1
Chixablox <smrdak152@gmail.com>
2023-07-03 09:55:08 +0000

    Начало работы

---
+++ SpaceCadets/Program.cs
@@ -0,0 +1,44 @@
+﻿﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft;
+using System.IO;
+using System.Linq;
+using System.CommandLine;
+
+namespace SpaceCadets
+{
+    class SpaceJson
+    {
+        public string taskName;
+        public SpaceCadets[] data;
+    }
+    class SpaceCadets
+    {
+        public string name;
+        public string group;
+        public string discipline;
+        public double mark;
+    }
+    class SpaceCadetsMark
+    {
+        static void Main(string[] args)
+        {
+
+            string inputPath = args[0];
+            string outputPath = args[1];
+
+            var json = JsonConvert.DeserializeObject<SpaceJson>(File.ReadAllText(inputPath));
+
+            if(json.taskName=="GetStudentsWithHighestGPA")
+            {
+                double highestGPA = 5;
+                var studentsWithHighestGPA = json.data
+                .Where(cadet=>(cadet.mark+cadet.mark/2==highestGPA))
+                .Select(cadet=>new {Name = cadet.name, GPA = cadet.mark })
+                .ToList<dynamic>();
+
+                File.WriteAllText(outputPath, JsonConvert.SerializeObject(studentsWithHighestGPA));
+            }
+        }
+    }
+}
